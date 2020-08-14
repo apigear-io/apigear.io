05634961593ef52fffc35ec1bbf32012a25a376d
Juergen Ryannel <juergen@ryannel.org>
2020-08-14 17:15:16 +0200

    mobile appbar

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -2,19 +2,32 @@
   <v-app-bar elevate-on-scroll fixed color="white">
     <v-btn text to="/" nuxt>
       <img class="mr-2" src="apigear.png" height="32" />
-      <b>ApiGear</b>
+      <b>ApiGearxx</b>
     </v-btn>
     <v-spacer></v-spacer>
-
-    <v-btn
-      v-for="link in links"
-      :key="link.title"
-      text
-      :to="link.to"
-      :href="link.href"
-    >
-      {{ link.title }}
-    </v-btn>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn
+        v-for="link in links"
+        :key="link.title"
+        text
+        :to="link.to"
+        :href="link.href"
+      >
+        {{ link.title }}
+      </v-btn>
+    </v-toolbar-items>
+    <v-menu bottom left close-on-click>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" class="hidden-md-and-up" v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(link, i) in links" :key="i" :to="link.to">
+          <v-list-tile-title>{{ link.title }}</v-list-tile-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
@@ -22,6 +35,7 @@
 export default {
   data() {
     return {
+      sidebar: false,
       links: [
         { title: 'Use Cases', to: '/cases' },
         { title: 'Markets', to: '/markets' },
