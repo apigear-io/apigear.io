777f1bcc192a642af0809ae05c416880da831a5c
Juergen Ryannel <juergen@ryannel.org>
2020-09-15 09:33:32 +0200

    initial changes

--- components/About.vue
+++ components/About.vue
@@ -2,11 +2,19 @@
   <v-row class="my-4">
     <v-col cols="12" sm="6">
       <v-card flat>
-        <v-card-subtitle>About ApiGear</v-card-subtitle>
+        <v-card-subtitle class="text-subtitle-1">About ApiGear</v-card-subtitle>
         <v-card-title class="text-h5">{{ body.title }}</v-card-title>
         <v-card-text>{{ body.text }}</v-card-text>
         <v-card-actions>
-          <v-btn outlined color="primary">Read More</v-btn>
+          <v-btn-toggle>
+            <v-btn
+              outlined
+              small
+              color="primary"
+              href="https://docs.apigear.io/guide/start"
+              >Read More</v-btn
+            >
+          </v-btn-toggle>
         </v-card-actions>
       </v-card>
     </v-col>
