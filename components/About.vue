dc7a109161245e56916d9f24440349cacb8bab48
Wolfgang Bremer <wolfgang@w-bremer.de>
2020-10-04 03:52:32 +0200

    About page fixed layout for different screen sizes

--- components/About.vue
+++ components/About.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-row>
-    <v-row class="my-4">
+  <v-container>
+    <v-row>
       <v-col cols="12" sm="6">
         <v-card flat>
-          <!-- <v-card-subtitle class="text-subtitle-1">About ApiGear</v-card-subtitle> -->
           <v-card-title class="text-h5">{{ body.title }}</v-card-title>
           <v-card-text>{{ body.text }}</v-card-text>
           <v-card-actions>
@@ -14,16 +13,17 @@
                 color="primary"
                 href="https://medium.com/apigear"
                 target="blank"
-                >Read our blog!</v-btn
+                >More on our blog</v-btn
               >
             </v-btn-toggle>
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="1" sm="1" align-self="center">
+      <v-col align="center" justify="center">
         <v-card flat>
+          <v-img src="apigear.png" width="100px" aspect-ratio="1"></v-img>
           <v-card-subtitle>Follow us on</v-card-subtitle>
-          <v-card-actions>
+          <v-card-actions class="justify-center">
             <v-btn
               v-for="(link, i) in body.social"
               :key="i"
@@ -37,42 +37,36 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="1" sm="1" align-self="center">
-        <v-img src="apigear.png" aspect-ratio="1"></v-img>
-      </v-col>
     </v-row>
-    <v-row>
-      <v-col :cols="12">
-        <v-container fluid>
-          <v-row dense>
-            <v-col v-for="member in body.team" :key="member.name" :cols="2">
-              <v-card max-width="250" min-width="250">
-                <v-img
-                  :src="member.src"
-                  class="white--text align-end"
-                  gradient="to bottom, rgba(0,0,0,.0), rgba(0,0,0,.3)"
-                  width="250px"
-                >
-                  <v-card-title v-text="member.name"></v-card-title>
-                </v-img>
-                <v-card-actions>
-                  <v-card-subtitle>{{ member.role }}</v-card-subtitle>
-                  <v-spacer></v-spacer>
-                  <v-btn icon target="blank" :href="member.linkedin">
-                    <v-icon>mdi-linkedin</v-icon>
-                  </v-btn>
-                </v-card-actions>
-                <v-card-text>
-                  <b>Why ApiGear?</b><br />
-                  {{ member.motivation }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-col>
-    </v-row>
-  </v-row>
+    <v-card flat>
+      <v-card-title class="text-h5">The team</v-card-title>
+      <v-row justify="center">
+        <v-col v-for="member in body.team" :key="member.name" md="4">
+          <v-card max-width="250" min-width="250">
+            <v-img
+              :src="member.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.0), rgba(0,0,0,.3)"
+              width="250px"
+            >
+              <v-card-title v-text="member.name"></v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-card-subtitle>{{ member.role }}</v-card-subtitle>
+              <v-spacer></v-spacer>
+              <v-btn icon target="blank" :href="member.linkedin">
+                <v-icon>mdi-linkedin</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-card-text class="justify-left">
+              <b>Why ApiGear?</b><br />
+              {{ member.motivation }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
