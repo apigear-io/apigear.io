e27b512f018b45da674a1dcd1fbbc412ec282d95
Juergen Ryannel <juergen@ryannel.org>
2020-10-05 16:05:42 +0200

    banner video

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,19 +1,33 @@
 <template>
   <v-row align="center" justify="center">
-    <v-col cols="12" sm="8">
+    <v-col cols="12" sm="6">
       <v-card flat class="my-4">
-        <v-card-title class="text-h2">{{ body.title }}</v-card-title>
+        <v-card-title class="text-h3">{{ body.title }}</v-card-title>
         <v-card-subtitle class="text-subtitle-1 my-4">{{
           body.text
         }}</v-card-subtitle>
+        <v-card-actions class="my-4">
+          <v-btn color="primary" href="https://app.apigear.io/-/register">
+            Get started for free
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
-    <v-col cols="12" sm="4">
-      <v-card-actions class="my-4">
-        <v-btn color="primary" href="https://app.apigear.io/-/register">
-          Sign up
-        </v-btn>
-      </v-card-actions>
+    <v-col cols="12" sm="6">
+      <v-card flat>
+        <v-card-text class="text-center">
+          <iframe
+            id="ytplayer"
+            type="text/html"
+            width="480"
+            height="270"
+            :src="
+              `https://www.youtube.com/embed/${body.video}?autoplay=1&origin=http://apigear.io`
+            "
+            frameborder="0"
+          ></iframe>
+        </v-card-text>
+      </v-card>
     </v-col>
   </v-row>
 </template>
