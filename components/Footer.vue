f2ed14b0541c7efd6c43e71ed5463914c18a374b
Wolfgang Bremer <wolfgang@w-bremer.de>
2020-10-06 23:57:10 +0200

    Footer: Change order of content
    Quick links should be more interessting and thus easier to see/access

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,6 +3,20 @@
     <v-row no-gutters>
       <v-col cols="6">
         <v-card outlined dark color="transparent">
+          <v-card-subtitle>Quick Links</v-card-subtitle>
+          <v-list dense color="transparent">
+            <v-list-item v-for="(link, i) in body.links" :key="i">
+              <v-list-item-subtitle>
+                <v-btn depressed x-small :href="link.href">
+                  {{ link.title }}
+                </v-btn>
+              </v-list-item-subtitle>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card outlined dark color="transparent">
           <v-card-subtitle
             >(c) {{ new Date().getFullYear() }} —
             <strong>{{ body.title }}</strong></v-card-subtitle
@@ -19,20 +33,6 @@
               <v-icon>mdi-{{ link.name }}</v-icon>
             </v-btn>
           </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col md="auto">
-        <v-card outlined dark color="transparent">
-          <v-card-subtitle>Quick Links</v-card-subtitle>
-          <v-list dense color="transparent">
-            <v-list-item v-for="(link, i) in body.links" :key="i">
-              <v-list-item-subtitle>
-                <v-btn depressed x-small :href="link.href">
-                  {{ link.title }}
-                </v-btn>
-              </v-list-item-subtitle>
-            </v-list-item>
-          </v-list>
         </v-card>
       </v-col>
     </v-row>
