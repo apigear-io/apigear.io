7deee616117feb8ffeb9847f67e959d907ce5c3b
Wolfgang Bremer <wolfgang@w-bremer.de>
2020-10-06 23:36:36 +0200

    Add legal notice
    The 'impressum' and others notes are legally mandatory.
    This is a first version which should be sufficient for now.
    
    This implements GEAR-33

---
+++ pages/legalnotice.vue
@@ -0,0 +1,18 @@
+<template>
+  <Legalnotice :body="body"></Legalnotice>
+</template>
+
+<script>
+import Legalnotice from '@/components/Legalnotice'
+export default {
+  components: {
+    Legalnotice
+  },
+  async asyncData({ $content }) {
+    const { body } = await $content('legalnotice').fetch()
+    return {
+      body
+    }
+  }
+}
+</script>
