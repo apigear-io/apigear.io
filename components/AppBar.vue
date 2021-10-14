753f621bd0272525c07e9d0046f9cdfcdfec3a7b
Juergen Ryannel <juergen@ryannel.org>
2021-10-14 16:50:23 +0200

    removed we::interface slogan

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -3,7 +3,6 @@
     <v-btn text to="/" nuxt active-class="no-active">
       <img class="mr-2" src="/apigear.png" height="32" />
       <b>ApiGear</b>
-      <span class="text-caption font-weight-thin ml-4">we::interface</span>
     </v-btn>
     <v-spacer></v-spacer>
     <AppMenuBar :items="items" class="hidden-sm-and-down" />
